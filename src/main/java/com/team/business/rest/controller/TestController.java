3677698d05f090a4460c91196ba3d540109ebde5
Your Name <you@example.com>
2016-10-09 20:27:02 +0800

    tem

--- src/main/java/com/team/business/rest/controller/TestController.java
+++ src/main/java/com/team/business/rest/controller/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.team.business.service.TestService;
-import com.team.business.util.Frequency;
+import com.team.business.util.annonations.Frequency;
 
 @Controller
 @Scope("prototype")
@@ -25,6 +25,12 @@ public class TestController {
 	@RequestMapping("first")
 	public String test1(){
 		testServcie1.test();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return "";
 	}
 	
