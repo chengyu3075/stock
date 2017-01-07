78864b331d6d44c554bf763780aaf277e9a1e8ce
Your Name <you@example.com>
2017-01-07 20:49:30 +0800

    修改名字

---
+++ src/main/java/com/team/business/dto/TeamResult.java
@@ -0,0 +1,42 @@
+package com.team.business.dto;
+
+import java.io.Serializable;
+
+public class TeamResult<T> implements Serializable {
+
+	private int resultCode;      //结果编码
+	private String resultInfo;   //结果信息
+	private T      data;         //结果
+	
+	public TeamResult(int resultCode, String resultInfo, T data) {
+		super();
+		this.resultCode = resultCode;
+		this.resultInfo = resultInfo;
+		this.data = data;
+	}
+	
+	public TeamResult(){
+		
+	}
+	
+	public int getResultCode() {
+		return resultCode;
+	}
+	public void setResultCode(int resultCode) {
+		this.resultCode = resultCode;
+	}
+	public String getResultInfo() {
+		return resultInfo;
+	}
+	public void setResultInfo(String resultInfo) {
+		this.resultInfo = resultInfo;
+	}
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
+	
+	
+}
